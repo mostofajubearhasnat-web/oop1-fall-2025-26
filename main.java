149a4bbc129cd37d8f553c666121f069ed66f656
mostofajubearhasnat-web <mostofajubearhasnat@gmail.com>
2025-10-28 23:17:26 +0600

    25-60887-1

---
+++ main.java
@@ -0,0 +1,31 @@
+
+ import java.util.*;
+
+ public class main {
+
+    
+
+  public static void main(String[] args) {
+
+   Scanner Sc = new Scanner(System.in);
+
+    System.out.println("Enter the marks of 3 students: ");
+
+    float marks1 = Sc.nextFloat();
+
+    float marks2 = Sc.nextFloat();
+
+    float marks3 = Sc.nextFloat();
+
+    
+
+    float sum = marks1+marks2+marks3;
+
+    float average = sum/3;
+
+
+    System.out.println(average );
+
+  }  
+
+}
