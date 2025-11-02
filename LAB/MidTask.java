3391051e12bfddcd5363a2832debacb7d855dca0
mostofajubearhasnat-web <mostofajubearhasnat@gmail.com>
2025-11-02 16:59:26 +0600

    25-60887-1

---
+++ LAB/MidTask.java
@@ -0,0 +1,19 @@
+public class MidTask{
+        public static void main(String[]args){
+            //Generate fullname
+            String firstname="mostofa";
+            String lastname= "jubear";
+            String fullname=firstname+"-"+lastname;
+            System.out.println(fullname);
+            //generate a productcode
+            String productname="iphone";
+            String productid="908090";
+            String productcode= productname+"-"+productid;
+            System.out.println(productcode);
+                
+            //add email adress
+            String email = "mostofajubear.turing@example.com";
+             // Find the index of the '@' symbol
+             int atIndex = email.indexOf('@');
+            }
+        }
