6345841e6c8cea38e31159984a3412a31360e972
mostofajubearhasnat-web <mostofajubearhasnat@gmail.com>
2025-11-02 14:51:23 +0600

    25-60887-1

---
+++ Task 2.java
@@ -0,0 +1,10 @@
+public class Q2 {
+    public static void main(String[] args) {
+        System.out.println("Enter Price :");
+        double cost = 250;
+        double discount = cost * 0.15;
+        double finalPrice = cost - discount;
+        System.out.println("Final price after 15% discount: $" + finalPrice);
+    }
+}
+
